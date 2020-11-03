84f5622c7a3788c3aacd69852cd9f425f0d90f2e
Erica <ericasu33@gmail.com>
2020-11-03 20:29:52 +0000

    desplays the char of a sentence one by one

---
+++ typewriter.js
@@ -0,0 +1,15 @@
+const sentence = "Hello there from lighthouse";
+
+let time = 0;
+let delay = 50;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, time);
+  time += delay;
+}
+
+setTimeout(() => {
+  process.stdout.write('\n');
+}, time + 10);
